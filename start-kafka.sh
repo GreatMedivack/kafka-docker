b3f1104a6d89e21de07bc5f4a59a75f28d56b8e8
GreatMedivack <seisoku.sento@gmail.com>
2020-02-13 10:18:22 +0500

    Update start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+rm "/kafka/kafka-logs-$HOSTNAME/meta.properties"
+
 # Allow specific kafka versions to perform any unique bootstrap operations
 OVERRIDE_FILE="/opt/overrides/${KAFKA_VERSION}.sh"
 if [[ -x "$OVERRIDE_FILE" ]]; then
